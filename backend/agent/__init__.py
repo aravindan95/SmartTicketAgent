0ff1a2ea3e123e97f4d53d50e6f4d748017966a1
aravindan95 <aravindan2695@gmail.com>
2026-01-09 08:22:15 +0530

    Update __init__.py

--- backend/agent/__init__.py
+++ backend/agent/__init__.py
@@ -1,5 +1,5 @@
 """Agent package for SmartTicket"""
 
-from .smart_agent import create_smart_ticket_agent, run_agent_conversation
+from smart_agent import create_smart_ticket_agent, run_agent_conversation
 
 __all__ = ['create_smart_ticket_agent', 'run_agent_conversation']
